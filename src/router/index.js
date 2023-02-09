3401dc05b77ee44b0979aab65962f9f850eabc31
Ibrokhim <izerit@mail.ru>
2023-02-09 16:56:25 +0500

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -32,11 +32,11 @@ const router = createRouter({
     },
   ],
   scrollBehavior(to, from, savedPosition) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({ left: 0, top: 0 })
-      }, 500)
-    })
+    // return new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve({ left: 0, top: 0 })
+    //   }, 500)
+    // })
     return { top: 0 }
   },
 });
