f73d6340e4591e14e3a753f9c93df8bd0cb62481
Ibrokhim <izerit@mail.ru>
2023-02-10 20:09:46 +0500

    Team section is ready

--- src/OtherSections/Team.vue
+++ src/OtherSections/Team.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="main">
-    <div class="profile-card">
+  <div class="text-center mt-5 mb-5 services_title">
+    <h2>Nextin Jamoasi</h2>
+  </div>
+  <div class="main d-flex justify-content-center mb-5 mt-5">
+    <div class="profile-card mb-5">
       <div class="img">
-        <img src="https://1.bp.blogspot.com/-8c7QTLoyajs/YLjr2V6KYRI/AAAAAAAACO8/ViVPQpLWVM0jGh3RZhh-Ha1-1r3Oj62wQCNcBGAsYHQ/s16000/team-1-3.jpg">
+        <img src="src/assets/team/shohruxTem.jpg">
+
       </div>
       <div class="caption">
-        <h3>Vin Diesel</h3>
-        <p>Senior App Developer</p>
+        <h3>Shohrux Malikov</h3>
+        <p>Nextin WS asoschisi, Back-end dasturchi.</p>
+
         <div class="social-links">
           <a href="#"><i class="fab fa-facebook"></i></a>
           <a href="#"><i class="fab fa-instagram"></i></a>
@@ -14,13 +19,13 @@
         </div>
       </div>
     </div>
-    <div class="profile-card">
+    <div class="profile-card mb-5">
       <div class="img">
-        <img src="https://1.bp.blogspot.com/-vhmWFWO2r8U/YLjr2A57toI/AAAAAAAACO4/0GBonlEZPmAiQW4uvkCTm5LvlJVd_-l_wCNcBGAsYHQ/s16000/team-1-2.jpg">
+        <img src="src/assets/team/MuhammadaminTeam.jpg">
       </div>
       <div class="caption">
-        <h3>David Corner</h3>
-        <p>Front End Developer</p>
+        <h3>Muhammadamin Shukuraliyev</h3>
+        <p>Back-end Developer</p>
         <div class="social-links">
           <a href="#"><i class="fab fa-facebook"></i></a>
           <a href="#"><i class="fab fa-instagram"></i></a>
@@ -28,13 +33,27 @@
         </div>
       </div>
     </div>
-    <div class="profile-card">
+    <div class="profile-card mb-5">
       <div class="img">
-        <img src="https://1.bp.blogspot.com/-AO5j2Y9lzME/YLjr3mxiqAI/AAAAAAAACPE/KAaYYTtQTrgBE3diTbxGoc4U4fCGx-C2gCNcBGAsYHQ/s16000/team-1-4.jpg">
+        <img src="src/assets/team/AbduqodirTeam.png">
       </div>
       <div class="caption">
-        <h3>Tom Cruise</h3>
-        <p>Full Stact Developer</p>
+        <h3>Abduqodir Khamroqulov</h3>
+        <p>Back-end Developer</p>
+        <div class="social-links">
+          <a href="#"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="profile-card mb-5">
+      <div class="img">
+        <img src="src/assets/team/ibrokhimTeam.png">
+      </div>
+      <div class="caption">
+        <h3>Ibrokhim Suleymanov</h3>
+        <p>Front-end Developer</p>
         <div class="social-links">
           <a href="#"><i class="fab fa-facebook"></i></a>
           <a href="#"><i class="fab fa-instagram"></i></a>
