9958425c82df0d0352c6b3982c3b5c5afca5c831
Ibrokhim <izerit@mail.ru>
2023-02-13 22:03:59 +0500

    team ready

--- src/OtherSections/Team.vue
+++ src/OtherSections/Team.vue
@@ -2,65 +2,69 @@
   <div class="text-center mt-5 mb-5 services_title">
     <h2>Nextin Jamoasi</h2>
   </div>
-  <div class="main d-flex justify-content-center mb-5 mt-5">
-    <div class="profile-card mb-5">
-      <div class="img">
-        <img src="/src/assets/team/shohruxTeam.jpg" alt="shohrux">
-      </div>
-      <div class="caption">
-        <h3>Shohrux Malikov</h3>
-        <p>Nextin WS asoschisi, Back-end dasturchi.</p>
+  <div class="container">
+    <div class="main row justify-content-center mb-5 mt-5 ">
+      <div class="profile-card mb-5 col-lg-3 col-md-6 col-sm-12">
+        <div class="img">
+          <img src="/src/assets/team/shohruxTeam.jpg" alt="shohrux">
+        </div>
+        <div class="caption">
+          <h3>Shohrux Malikov</h3>
+          <p>Nextin WS asoschisi, Back-end dasturchi.</p>
 
-        <div class="social-links">
-          <a href="#"><i class="fab fa-facebook"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="profile-card mb-5">
-      <div class="img">
-        <img src="/src/assets/team/MuhammadAminTeam.jpg" alt="MuhammadAmin">
-      </div>
-      <div class="caption">
-        <h3>Muhammadamin Shukuraliyev</h3>
-        <p>Back-end Developer</p>
-        <div class="social-links">
-          <a href="#"><i class="fab fa-facebook"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
+      <div class="profile-card mb-5 col-lg-3  col-md-6 col-sm-12">
+        <div class="img">
+          <img src="/src/assets/team/MuhammadAminTeam.jpg" alt="MuhammadAmin">
+        </div>
+        <div class="caption">
+          <h3>Muhammadamin Shukuraliyev</h3>
+          <p>Back-end Developer</p>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="profile-card mb-5">
-      <div class="img">
-        <img src="/src/assets/team/AbduqodirTeam.png" alt="Abduqodir">
-      </div>
-      <div class="caption">
-        <h3>Abduqodir Khamroqulov</h3>
-        <p>Back-end Developer</p>
-        <div class="social-links">
-          <a href="#"><i class="fab fa-facebook"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
+      <div class="profile-card mb-5 col-lg-3 col-md-6 col-sm-12">
+        <div class="img">
+          <img src="/src/assets/team/AbduqodirTeam.png" alt="Abduqodir">
+        </div>
+        <div class="caption">
+          <h3>Abduqodir Khamroqulov</h3>
+          <p>Back-end Developer</p>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="profile-card mb-5">
-      <div class="img">
-        <img src="/src/assets/team/ibrokhimTeam.png" alt="ibrokhim">
-      </div>
-      <div class="caption">
-        <h3>Ibrokhim Suleymanov</h3>
-        <p>Front-end Developer</p>
-        <div class="social-links">
-          <a href="#"><i class="fab fa-facebook"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-          <a href="#"><i class="fab fa-twitter"></i></a>
+      <div class="profile-card mb-5 col-lg-3 col-md-6 col-sm-12">
+        <div class="img">
+          <img src="/src/assets/team/ibrokhimTeam.png" alt="ibrokhim">
+        </div>
+        <div class="caption">
+          <h3>Ibrokhim Suleymanov</h3>
+          <p>Front-end Developer</p>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+          </div>
         </div>
       </div>
     </div>
   </div>
+
+
 </template>
 
 <script>
