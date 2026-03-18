4048f118d51d669a17d1181b98609f4c9f110eaf
Ibrokhim <izerit@mail.ru>
2026-03-19 01:58:47 +0500

    added titles

--- src/router/index.js
+++ src/router/index.js
@@ -8,52 +8,82 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomePage
+      component: HomePage,
+      meta: {
+        title: "Nextin | Veb-studiya"
+      }
     },
     {
       path: "/xizmatlar/web-design",
       name: "xizmatlar-web-design",
-      component : ()=> import('../OtherSections/Services/ServicesDesign.vue')
+      component : ()=> import('../OtherSections/Services/ServicesDesign.vue'),
+      meta: {
+        title: "Nextin | Veb-dizayn"
+      }
     },
     {
       path: "/xizmatlar/web-dasturlash",
       name: "xizmatlar-web-dasturlash",
-      component : ()=> import('../OtherSections/Services/ServicesDasturlash.vue')
+      component : ()=> import('../OtherSections/Services/ServicesDasturlash.vue'),
+      meta: {
+        title: "Nextin | Veb-dasturlash"
+      }
     },
     {
       path: "/xizmatlar/seo-optimizatsiya",
       name: "xizmatlar-seo-optimizatsiya",
-      component : ()=> import('../OtherSections/Services/ServicesSeoOpt.vue')
+      component : ()=> import('../OtherSections/Services/ServicesSeoOpt.vue'),
+      meta: {
+        title: "Nextin | SEO optimizatsiya"
+      }
     },
     {
       path: "/xizmatlar/google-ads",
       name: "xizmatlar-google-ads",
-      component : ()=> import('../OtherSections/Services/ServicesGoogleAds.vue')
+      component : ()=> import('../OtherSections/Services/ServicesGoogleAds.vue'),
+      meta: {
+        title: "Nextin | Google reklamalari"
+      }
     },
     {
       path: "/xizmatlar/email-marketing",
       name: "xizmatlar-email-marketing",
-      component : ()=> import('../OtherSections/Services/ServicesEmailMarketing.vue')
+      component : ()=> import('../OtherSections/Services/ServicesEmailMarketing.vue'),
+      meta: {
+        title: "Nextin | Email marketing"
+      }
     },
     {
       path: "/xizmatlar/internet-do-kon",
       name: "xizmatlar-internet-do-kon",
-      component : ()=> import('../OtherSections/Services/ServicesOnlineStore.vue')
+      component : ()=> import('../OtherSections/Services/ServicesOnlineStore.vue'),
+      meta: {
+        title: "Nextin | Internet do'kon"
+      }
     },
     {
       path: "/xizmatlar/internet-marketing",
       name: "xizmatlar-internet-marketing",
-      component : ()=> import('../OtherSections/Services/ServicesInternetMarketing.vue')
+      component : ()=> import('../OtherSections/Services/ServicesInternetMarketing.vue'),
+      meta: {
+        title: "Nextin | Internet marketing"
+      }
     },
     {
       path: "/jamoa",
       name: "jamoa",
-      component : ()=> import('../OtherSections/Team.vue')
+      component : ()=> import('../OtherSections/Team.vue'),
+      meta: {
+        title: "Nextin | Jamoa"
+      }
     },
     {
       path: "/narxlar_xizmatlar",
       name: "narx",
-      component : ()=> import('../OtherSections/Price.vue')
+      component : ()=> import('../OtherSections/Price.vue'),
+      meta: {
+        title: "Nextin | Narxlar"
+      }
     },
   ],
   scrollBehavior(to, from, savedPosition) {
@@ -66,6 +96,18 @@ const router = createRouter({
 
     return { top: 0 }
   },
+});
+
+// Global navigation guard to update page title
+router.beforeEach((to, from, next) => {
+  // Set document title from route meta
+  if (to.meta && to.meta.title) {
+    document.title = to.meta.title;
+  } else {
+    // Fallback title if no meta title is set
+    document.title = "Nextin | Veb-studiya";
+  }
+  next();
 });
 
 export default router;
